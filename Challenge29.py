ead612ea52973946235d07a1ec969b40972ede6d
wamballa <steve.tyrell@gmail.com>
2015-09-12 22:19:40 +0100

    uploading challenge 29

---
+++ Challenge29.py
@@ -0,0 +1,12 @@
+__author__ = 'wamballa'
+
+"""
+Challenge 28
+
+Write a program that allows the user to create and store a checklist for a holiday.
+It should start by asking them the destination of the holiday, how many things they
+need to pack and how many tasks they need to complete to prepare.
+
+The user should then be able to enter each item they need to pack and each task they need to complete.
+"""
+
