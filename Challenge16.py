83c20285ef35c06d6a4e76a90c8dddd18409ef2d
wamballa <steve.tyrell@gmail.com>
2015-09-20 17:30:34 +0100

    uploading challenge 16

--- Challenge16.py
+++ Challenge16.py
@@ -9,4 +9,4 @@ outputs all numbers between 1 and 10
 print ("Challenge 16: Output all numbers between 1 and 10")
 
 for i in range ( 1, 11):
-    print ("%s " % i,end="")
+    print ("%s " % i,end="") # ,end="" prevents printing to a new line
