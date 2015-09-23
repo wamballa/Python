b76a2a00e471e5b60b32d12b2d68c702e6dfb927
wamballa <steve.tyrell@gmail.com>
2015-09-23 16:54:13 +0100

    uploading challenge 17

--- Challenge17.py
+++ Challenge17.py
@@ -10,5 +10,5 @@ print ("Challenge 17: output all odd numbers between 1 and 99")
 
 for i in range ( 1, 100):
 
-    if i % 2 != 0: # a % b calculate the remainder of dividing a by b. Odd numbers always have a remainder if divided by 2
+    if i % 2 != 0: # a % b calculate the remainder of dividing a by b. Odd numbers always have a remainder if divided by 2+
         print ("%s " % i,end="")
---
+++ Nought&Crosses.py
@@ -0,0 +1,113 @@
+"""
+
+Noughts and Crosses
+
+"""
+
+
+slot = ["-","-","-","-","-","-","-","-","-"]
+numCols = 3
+numRows = 3
+gameOver = False
+
+def drawBoard():
+    print ("Slot number    Game Board")
+    for row in range ( numRows ):
+        rowStart = row*numCols +1
+        print ("%s %s %s          " % ( rowStart, rowStart+1,rowStart+2),end="")
+        for col in range ( numCols ):
+            print ("%s  " % slot[( row * numCols)+col],end="")
+        print("")
+
+def resetBoard():
+    global slot
+    slot = ["-","-","-","-","-","-","-","-","-"]
+    #drawBoard()
+
+def checkWin():
+    global gameOver
+
+    # check if any rows are matching
+    for row in range ( numRows ):
+        checkSlot = row*numCols
+        if slot[checkSlot]!="-" and slot[checkSlot] == slot[checkSlot+1] and slot[checkSlot+1]==slot[checkSlot+2]:
+            gameOver = True
+            if slot[checkSlot]=="X":
+                print ("\n Congrats. Player X has won the game by capturing a row.")
+                drawBoard()
+            else:
+                print ("\n Congrats. Player X has won the game by capturing a row.")
+                drawBoard()
+            resetBoard()
+            return
+
+    # check if any columns are matching
+    for col in range (numCols ):
+        checkSlot = col #*numRows
+        if slot[checkSlot]!="-" and slot[checkSlot] == slot[checkSlot+numRows] and slot[checkSlot+numRows]==slot[checkSlot+numRows*2]:
+            gameOver = True
+            if slot[checkSlot]=="X":
+                print ("\n Congrats. Player X has won the game by capturing a column.")
+                drawBoard()
+            else:
+                print ("\n Congrats. Player O has won the game by capturing a column.")
+                drawBoard()
+            resetBoard()
+            return
+
+    # check if any diagonal \ are matching
+    if slot[0] != "-":
+        if slot[0] == slot[4] and slot[4] == slot[8]:
+            gameOver = True
+            if slot[0] == "X":
+                print ("\n Congrats. Player X has won the game by capturing a diagonal.")
+                drawBoard()
+            else:
+                print ("\n Congrats. Player O has won the game by capturing a diagonal.")
+                drawBoard()
+            resetBoard()
+            return
+    # check if any diagonal / are matching
+    if slot[2] != "-":
+        if slot[2] == slot[4] and slot[4] == slot[6]:
+            gameOver = True
+            if slot[2] == "X":
+                print ("\n Congrats. Player X has won the game by capturing a diagonal.")
+                drawBoard()
+            else:
+                print ("\n Congrats. Player O has won the game by capturing a diagonal.")
+                drawBoard()
+            resetBoard()
+            return
+# Main
+
+drawBoard()
+
+while True:
+    while True:
+        playerMove = int ( input ("Player X: Enter slot number: "))
+        if slot[playerMove -1] == "-":
+            slot[playerMove -1] = "X"
+            checkWin()
+            if not gameOver:
+                drawBoard()
+            break
+        else:
+            print ("space taken")
+
+    while True and not gameOver:
+        playerMove = int ( input ("Player O: Enter slot number: "))
+        if slot[playerMove -1] == "-":
+            slot[playerMove -1] = "O"
+            checkWin()
+            if not gameOver:
+                drawBoard()
+            break
+        else:
+            print ("space taken")
+    if gameOver == True:
+        gameOver = False
+        print ("\nStart Another Game...clear the board!!")
+        drawBoard()
+
+
