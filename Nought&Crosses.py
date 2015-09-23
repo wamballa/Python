bef806e0acfa3f0f721d99b300a83f362f8c1a6c
wamballa <steve.tyrell@gmail.com>
2015-09-23 19:40:04 +0100

    noughts and crosses

--- Nought&Crosses.py
+++ Nought&Crosses.py
@@ -2,27 +2,47 @@
 
 Noughts and Crosses
 
+Overview:
+
+Players: 2 players = X and O
+Inputs: each player enters a slot number to put X or O into.Slots arranged:
+1 2 3
+4 5 6
+7 8 9
+Variables: slot[] = list length is 9 [0..8] and holds contents for each slot. Either - (empty), X or O
+
+Functions:
+drawBoard() - draws the slot numbers the user must enter and the contents of each slot ( from list called slot )
+resetBoard() - clears the list called slot filling it with -
+checkWin() - check if player has 3 in a row, column or diagonal /\
+
+
 """
 
-
+# Setup global variables
 slot = ["-","-","-","-","-","-","-","-","-"]
 numCols = 3
 numRows = 3
 gameOver = False
 
+# draw board in two parts. left is guide for the slot number, right is the game board
 def drawBoard():
     print ("Slot number    Game Board")
-    for row in range ( numRows ):
-        rowStart = row*numCols +1
+    # prints row by row
+    for row in range ( numRows ): # go row by row
+        # first draw the number of the slots user needs to enter
+        rowStart = row*numCols +1 #calculate
+
+
         print ("%s %s %s          " % ( rowStart, rowStart+1,rowStart+2),end="")
         for col in range ( numCols ):
             print ("%s  " % slot[( row * numCols)+col],end="")
         print("")
 
+# sets the slot list to empty = replaces each slot with a -
 def resetBoard():
-    global slot
+    global slot # cannot change a global variable in a function without this
     slot = ["-","-","-","-","-","-","-","-","-"]
-    #drawBoard()
 
 def checkWin():
     global gameOver
@@ -36,7 +56,7 @@ def checkWin():
                 print ("\n Congrats. Player X has won the game by capturing a row.")
                 drawBoard()
             else:
-                print ("\n Congrats. Player X has won the game by capturing a row.")
+                print ("\n Congrats. Player O has won the game by capturing a row.")
                 drawBoard()
             resetBoard()
             return
@@ -67,6 +87,7 @@ def checkWin():
                 drawBoard()
             resetBoard()
             return
+
     # check if any diagonal / are matching
     if slot[2] != "-":
         if slot[2] == slot[4] and slot[4] == slot[6]:
@@ -79,11 +100,14 @@ def checkWin():
                 drawBoard()
             resetBoard()
             return
-# Main
 
-drawBoard()
 
-while True:
+# Main loop - start game
+
+drawBoard() # draw the board to start
+
+while True: # infinit game loop / no end to the game
+
     while True:
         playerMove = int ( input ("Player X: Enter slot number: "))
         if slot[playerMove -1] == "-":
@@ -95,7 +119,7 @@ while True:
         else:
             print ("Slot taken!! Try again...")
 
-    while True and not gameOver:
+    while True and not gameOver: # dont ask for input from player if game is over / True
         playerMove = int ( input ("Player O: Enter slot number: "))
         if slot[playerMove -1] == "-":
             slot[playerMove -1] = "O"
@@ -105,6 +129,8 @@ while True:
             break
         else:
             print ("Slot taken!! Try again...")
+
+
     if gameOver == True:
         gameOver = False
         print ("\nStart Another Game...clear the board!!")
