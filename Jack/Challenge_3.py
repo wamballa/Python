84f4c93e8018d297737bed5d7e32a19b15d25619
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:03:53 +0100

    Challenge 2 by jack

---
+++ Jack/Challenge_3.py
@@ -0,0 +1,12 @@
+"""
+Challenge 3
+
+Write a program that:
+asks the user to input the width and height of a rectangle
+calculates the area
+outputs the result
+"""
+width=int(input("What is the width of the RECTANGLE"))
+length=int(input("What is the length of the RECTANGLE"))
+sum=area=(width*length)
+print("The area is",area)
