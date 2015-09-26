c245e40145d8dce3a0514ca7918ea986b6a41c60
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:21:32 +0100

    Challenge 11 by jack

---
+++ Jack/Challenge_11.py
@@ -0,0 +1,12 @@
+"""
+Challenge 11
+
+Write a program that:
+asks the user to input a sentence
+calculates and outputs how many characters there are in the sentence
+"""
+print ("Challenge 11: How long is your sentence?")
+
+string = input ("Enter your sentence now: ")
+
+print ("Your sentence is %s characters long" % len(string))
