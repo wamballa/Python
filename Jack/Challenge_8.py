1160934239ca673be150aab3a601a51c80cc85da
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:12:40 +0100

    Challenge 8 by jack

---
+++ Jack/Challenge_8.py
@@ -0,0 +1,16 @@
+"""
+Challenge 8
+
+Write a program that:
+converts and outputs marks into grades, using the following data:
+"""
+score = int ( input ("Please enter your score: "))
+
+if score >= 75:
+    print (" You got an A")
+elif score >=60 and score < 75:
+    print ("You got a B")
+elif score >= 35 and score < 60:
+    print ("You got a C")
+elif score < 35:
+    print ("You got a D for dumbass")
