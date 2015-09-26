0f83f2787dadf1b2b30a5c1fab7338edffe25fce
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:30:55 +0100

    Challenge 18 by jack

---
+++ Jack/Challenge_18.py
@@ -0,0 +1,12 @@
+"""
+Challenge 18
+
+Write a program that:
+asks the user to input a number
+outputs the times table for that number
+"""
+print ("Times Table ")
+
+table = int(input("Enter the number of the times table to show: "))
+for i in range (1, 13): # repeat 1 to 12
+    print ("%s x %s = %s" % (i,table,(i*table)))
