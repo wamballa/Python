7b738859ff4078cf048f74eb50632d957f5914dd
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:07:14 +0100

    Challenge 4 by jack

---
+++ Jack/Challenge_5.py
@@ -0,0 +1,11 @@
+"""
+Challenge 5
+
+Write a program that:
+asks the user their name
+asks what their favourite subject is (using their name in the question)
+responds to their answer by saying that you like that subject as well
+"""
+name=input("What is your name?")
+Subject=input(str(name+" What is your favourite subject"))
+print("I like",Subject, "aswell")
