f903e1b400abea817a9fd4daed09543019dbc61f
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:01:46 +0100

    Challenge 2 by jack

---
+++ Jack/Challenge_2.py
@@ -0,0 +1,13 @@
+"""
+Challenge 2
+
+Write a program that:
+asks the user to input two numbers
+calculates the average
+outputs the result
+"""
+number1=int(input("Pick a number"))
+number2=int(input("Pick another number"))
+sum=total=(number1+number2)
+print("The total is",total)
+
