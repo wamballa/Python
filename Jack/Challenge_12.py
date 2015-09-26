17ebc78cfb84d2e2b4849d26c1db4f3eb17d0d06
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:22:50 +0100

    Challenge 12 by jack

---
+++ Jack/Challenge_12.py
@@ -0,0 +1,13 @@
+"""
+Challenge 12
+
+Write a program that:
+asks the user to input a sentence
+outputs the sentence in upper case
+"""
+print ("Challenge15: Convert sentence to UPPER CASE")
+
+string = input ("Enter your sentence: ")
+string = string.upper()
+
+print ("Your string in UPPER CASE: %s" % string)
