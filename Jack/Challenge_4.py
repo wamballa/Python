e11004e5ab992a36e5ae49653fa7eef217933c34
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:05:27 +0100

    Challenge 4 by jack

---
+++ Jack/Challenge_4.py
@@ -0,0 +1,12 @@
+"""
+Challenge 4
+
+Write a program that:
+asks the user to input two numbers
+divides the first number by the second number
+outputs the result
+"""
+number1=int(input("Pick a number to divide"))
+number2=int(input("Pick a second number to divide"))
+sum=total=(number1//number2)
+print("The answer is", total)
