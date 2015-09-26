aee51e6ca1c15a1f2c0e5f4c0e84ba99ec1804ae
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:08:40 +0100

    Challenge 6 by jack

---
+++ Jack/Challenge_6.py
@@ -0,0 +1,12 @@
+"""
+Challenge 6
+
+Write a program that:
+asks the user their name
+if it is the same as your name, outputs 'You’re cool', otherwise outputs 'Nice to meet you'
+"""
+name=input("What's your name?")
+if name=="Jack":
+    print("Your Cool")
+elif name!="":
+     print("Nice to meet you")
