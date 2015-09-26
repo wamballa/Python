474a9740d6509fdca687e5d33cb30a9965283658
JackTyrell <jackhtyrell@gmail.com>
2015-09-26 18:32:15 +0100

    Challenge 19 by jack

---
+++ Jack/Challenge_19.py
@@ -0,0 +1,14 @@
+"""
+Challenge 19
+
+Write a program that:
+asks the user to input a number
+outputs the times table for that number
+starts again every time it finishes
+"""
+print ("Times Table ")
+
+while True: # create a loop that starts again every time it finishes
+    table = int(input("Enter the number of the times table to show: "))
+    for i in range (1, 13): # repeat 1 to 12
+        print ("%s x %s = %s" % (i,table,(i*table)))
